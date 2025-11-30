f112c9ceb8456b1ea0d7594ca2178bcd2c4f7961
Krecior4 <he.klocuszek123@gmail.com>
2025-11-30 18:30:22 +0100

    Whole app done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CatScroller from './CatScroller'
+
+function App() {
+  return (
+    <>
+      <CatScroller/>
+    </>
+  )
+}
+
+export default App
---
+++ src/CatScroller.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState, useRef } from "react"
+import './CatScroller.css'
+
+export default function CatScroller() {
+    const [url, setUrl] = useState<string>()
+    const [clicked, setClicked] = useState<boolean>()
+    let meh = useRef(new Audio("../assets/meh.wav"))
+    let cute = useRef(new Audio("../assets/cute.wav"))
+
+    const reloadCat = async () => {
+        const response = await fetch("https://cataas.com/cat")
+        const result = await response.blob()
+        const internalUrl = URL.createObjectURL(result)
+        setUrl(internalUrl)
+        await setClicked(false)
+    }
+
+    useEffect(() => {
+        reloadCat()
+    }, [])
+
+    return (
+        <>
+            <h1>Let's watch some cute cats!</h1>
+            <div id="content-wrapper">
+                <img id="img" src={url} alt="Cute cat" />
+                <div id="controllers">
+                    <button onClick={() => {
+                        meh.current.play()
+                        setClicked(true)
+                        setTimeout(() => {
+                            reloadCat()
+                        }, 800)
+                    }} disabled={clicked}>Meh</button>
+                    <button onClick={() => {
+                        cute.current.play()
+                        setClicked(true)
+                        setTimeout(() => {
+                            reloadCat()
+                        }, 800)
+                    }} disabled={clicked}>Cute!</button>
+                </div>
+            </div>
+        </>
+    )
+}
